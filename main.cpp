705d478f8b56517f5f2a911fb845c604e1bde854
xyith <jonasz.stysiak@gmail.com>
2025-09-08 19:59:12 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "headers/headers.h"
 #include <chrono>
+/*
+MOZNA BY ZMIENIC TE WSZYSTKIE VECTOR2ADD ITD NA WLASNA MATEMATYKE I ZROBIC Z TEGO NOWY HEADER
+PISAC POLOZENIE I MOZE PREDKOSC NA KULKACH
 
-
+rigid body, new struct, velocity vector?, only need to find firs occurance of rectangle char
+in matrix, simulate it as a whole
+*/
 
 int main(void){
     
     
-    cout<<"\n Startup test \n \n";
+    cout<<"\n check no1 \n \n";
     auto startTime = chrono::high_resolution_clock::now();
     
     InitWindow(screenWidth, screenHeight, "Physics2D");
@@ -37,7 +42,7 @@ int main(void){
             float runtime = elapsed.count();
             
             
-            
+            int fps = GetFPS();
             tick++;
             float mousex = float(GetMouseX());
             float mousey = float(GetMouseY());
@@ -46,13 +51,10 @@ int main(void){
             
             //initializing physics
             for (auto& obj : circles) {
-                obj.APPLYFORCE(GRAVITY);                  
-                obj.ISONGROUND(BARRIERS);        
-                obj.APPLYFRICTION();           
-                obj.UPDATEPOSITION();           
-                obj.DETECTBARRIERS(BARRIERS, 0.8f); 
+                obj.APPLYFORCE(g.GRAVITY);
+                obj.UPDATEPOSITION();
+                obj.DETECTBARRIERS(BARRIERS, RESTITUTION);
                 obj.PULLOBJ(mouseVec);
-
                 for (auto& sobj : circles) {
                     CIRCLECOLLISION(obj, sobj);
                 }
@@ -60,8 +62,8 @@ int main(void){
             
             
             t.UPDATE();
+            g.UPDATE();
             
-
 
             for (auto& obj : rigid) {
               obj.MAKERIGID(mouseVec);
@@ -71,25 +73,26 @@ int main(void){
 //Drawing -----------------------------------------------------------------------------------------------------------------
             BeginDrawing();
                 ClearBackground(BLACK);
-                DrawFPS(20, 20); 
-                DrawText(TextFormat("Run time: %.2f s", runtime), 20, 40, 20, GREEN);
+                DrawText(TextFormat("FPS: %d", fps), 20, 20, 20, DARKBLUE); 
+                DrawText(TextFormat("Run time: %.2f s", runtime), 20, 40, 20, DARKBLUE);
                 int fakeNumerator;
                 if(t.deltaTime == 0.f) fakeNumerator = 0;
                 else fakeNumerator = 1;
-                DrawText(TextFormat("Delta time: %d/%d", fakeNumerator, t.denominator), 20, 60, 20, GREEN);
-                DrawText(TextFormat("Mouse position: %.f - %.f", mousex, mousey), 20, 80, 20, GREEN);
-                DrawText(TextFormat("Restitution: %.2f", RESTITUTION), 20, 100, 20, GREEN);
-                DrawText(TextFormat("Gravity: %.2f", GRAVITY.y/100), 20, 120, 20, GREEN);
+                DrawText(TextFormat("Delta time: %d/%d", fakeNumerator, t.denominator), 20, 60, 20, DARKBLUE);
+                DrawText(TextFormat("Mouse position: %.f - %.f", mousex, mousey), 20, 80, 20, DARKBLUE);
+                DrawText(TextFormat("Restitution: %.2f", RESTITUTION), 20, 100, 20, DARKBLUE);
+                DrawText(TextFormat("GravityY: %.2f", g.GRAVITY.y/100), 20, 120, 20, DARKBLUE);
+                DrawText(TextFormat("GravityX: %.2f", g.GRAVITY.x/100), 20, 120, 20, DARKBLUE);
 
                 
                 
                 for (auto& obj : circles) {
                     DrawCircleV(obj.position, obj.radius, WHITE);
                     
-                    float speedx = obj.velocity.x;  
-                    float speedy = obj.velocity.y;   if (obj.onGround) speedy = 0.f;
-                    float speed = sqrt(speedx * speedx + speedy * speedy);
+                    float speedx = obj.velocity.x;   if (speedx < 1.0f && speedx > -1.0f) speedx = 0;
+                    float speedy = obj.velocity.y + 7;   if (speedy < 1.0f && speedy > -1.0f) speedy = 0;
                     
+                    float speed = sqrt(speedx * speedx + speedy * speedy);
                     
                     
                     DrawText(TextFormat("V:%.f", speed), obj.position.x - 25, obj.position.y - 25, 20, BLACK);
@@ -128,7 +131,7 @@ int main(void){
     }
     
     CloseWindow();
-    TESTSRESULTS();
     return 0;
     
+
 }
---
+++ simulationSettings.hpp
@@ -0,0 +1,63 @@
+#ifndef SIMULATIONSETTINGS_H
+#define SIMULATIONSETTINGS_H
+#include "const.hpp"
+
+
+// Z: tiome slow down, X: time speed up, C: reset
+// V: gravity slow down, B: gravity speed up, M: gravity left, N: gravity right
+// D: balls, RMB: rigid, Q: balls clear
+
+
+class timeController {
+  public:
+    int numerator = 1;
+    int denominator = 60;
+    float deltaTime = float(numerator) / float(denominator);
+    bool timeStopped = false;
+
+    void UPDATE() {
+        // manipulate time
+        if (IsKeyDown(KEY_Z)) denominator += 1;
+        if (IsKeyDown(KEY_X)) denominator -= 1;
+        if (IsKeyDown(KEY_C)) denominator = 60;
+        if (denominator < 1) denominator = 1; // safety so there is no x/0 
+
+        // stop time
+        if (IsKeyPressed(KEY_SPACE)) {
+            timeStopped = !timeStopped; // toggle
+        }
+
+        RecalculateDeltaTime();
+    }
+
+    void RecalculateDeltaTime() {
+        deltaTime = timeStopped ? 0.0f : float(numerator) / float(denominator);
+        //if true use 0.0f if false use the rest after :
+    }
+};
+ 
+
+class gravityController { 
+  public:
+    Vector2 GRAVITY = {0.f, 980.f};
+    
+    void UPDATE() {
+        
+        // manipulate GRAVITY left / right included
+        if (IsKeyDown(KEY_V)) GRAVITY.y += 10.f;
+        if (IsKeyDown(KEY_B)) GRAVITY.y -= 10.f;
+        if (IsKeyDown(KEY_N)) GRAVITY.x -= 10.f;
+        if (IsKeyDown(KEY_M)) GRAVITY.x += 10.f;
+
+        
+        if (IsKeyDown(KEY_C)){
+            GRAVITY = {0.f, 980.f};
+        }
+    }   
+};
+
+
+timeController t;
+gravityController g;
+
+#endif
